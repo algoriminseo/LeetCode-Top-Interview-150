22a582c4edee813b1eb6e8627dc1e7a14f76b229
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-21 18:32:01 -0800

    Time: 71 ms (71.65%), Space: 26.9 MB (33.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,15 @@
+#Time Complexity : O(n), where n is length of array Comparing with two pointers and worst case, it will access through all items in the array 
+#Space Complexity : O(1), since it does not need extra space
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_val = prices[0]
-        max_profit = 0
+        buy = prices[0]
+        max_profit = 0 
 
         for price in prices[1:]:
-            min_val = min(min_val, price)
-            max_profit = max(max_profit, price - min_val)
-           
+            if buy > price:
+                buy = price
+                
+            max_profit = max(max_profit, price - buy )
 
         return max_profit
