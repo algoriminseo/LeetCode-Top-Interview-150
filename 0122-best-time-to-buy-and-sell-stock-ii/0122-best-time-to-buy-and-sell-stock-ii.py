47cf9de7212f90955359e56ba18356f194516682
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-21 19:23:52 -0800

    Time: 4 ms (17.83%), Space: 18.7 MB (69.51%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,12 @@
+#Time Complexity : O(n) , where n is length of prices array
+#Space Compelxity : O(1) , since it does not need extra space
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0  
+        for i in range(1, len(prices)):
+            if (prices[i] - prices[i-1]) > 0 :
+                profit += prices[i] - prices[i-1]
+        
+        return profit 
