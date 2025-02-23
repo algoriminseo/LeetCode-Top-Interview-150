b96cdc315d984f2322328384bf37212397c69bda
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-23 05:20:31 -0800

    Time: 7 ms (79.41%), Space: 18.5 MB (74.31%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,12 +1,17 @@
+# Referenced Other codes and tried by myself
+# Algorithm : The main thing we need to know in this problem is that to get minimum jumps to nums[n-1],
+# it is core approach to think of maximum distance to get through
+# Time Complexity : O(n), where n is length of nums array
+# Space Complexity : O(1), since the function states about the variables
+
 class Solution:
 
-    
     def jump(self, nums: List[int]) -> int:
         near = 0
         far = 0 
-        jumps = 0
+        jumps = 0   
 
-        while far < len(nums) - 1:
+        while far < len(nums) - 1 :
             farthest = 0
             for i in range(near, far + 1):
                 farthest = max(farthest, i + nums[i])
@@ -15,4 +20,5 @@ class Solution:
             far = farthest
             jumps += 1
 
-        return jumps
+        return jumps 
+
