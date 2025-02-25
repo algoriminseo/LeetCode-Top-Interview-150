9dfb57c3a6d145d604099ddcb7a6b5d621387a1e
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-25 15:29:19 -0800

    Time: 151 ms (44.04%), Space: 57.9 MB (65.66%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,37 +1,37 @@
+#Time Complexity : Average O(1)
+#Space Compelxity : O(n)
 import random
 class RandomizedSet:
 
     def __init__(self):
-        self.data_map = {}
-        self.data = []
+        self.nums_map = {}
+        self.nums = []
 
     def insert(self, val: int) -> bool:
-        if val in self.data_map:
+        if val in self.nums_map:
             return False
-        self.data_map[val] = len(self.data)
-        self.data.append(val)
+        self.nums_map[val] = len(self.nums)
+        self.nums.append(val)
         return True
-
 
     def remove(self, val: int) -> bool:
-        if not val in self.data_map:
+        if not val in self.nums_map:
             return False
-        last_element_in_list = self.data[-1]
-        index_of_element_to_remove = self.data_map[val]
-        self.data_map[last_element_in_list] = index_of_element_to_remove
-        self.data[index_of_element_to_remove] = last_element_in_list
+        last_elem = self.nums[-1]
+        index_to_remove_elements = self.nums_map[val]
+        self.nums_map[last_elem] = index_to_remove_elements
+        self.nums[index_to_remove_elements] = last_elem
 
-        self.data.pop()
-        self.data_map.pop(val)
-
+        self.nums[-1] = val
+        self.nums.pop()
+        self.nums_map.pop(val)
         return True
-
-
 
 
     def getRandom(self) -> int:
-        rand_num = random.choice(self.data)
+        rand_num = random.choice(self.nums)
         return rand_num
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
