50fbc0cbc08dd1b3d574f2b6681d7bd58656a606
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-27 15:18:34 -0800

    Time: 12 ms (40.57%), Space: 34.8 MB (20.79%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,13 @@
+#Time Compelxity : O(n), where n is length of nums
+#Space Compelxity : O(n), where n is length of num2. In the worst case, if there is no duplicates, nums2 list will be created same as nums list.
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums2 = list(set(nums))
-
-        if len(nums) > len(nums2):
-            return True
+        hash_table = {}
+        for i in nums:
+            if i in hash_table.keys():
+                return True
+            else :         
+                hash_table[i] = i
         return False
-        
