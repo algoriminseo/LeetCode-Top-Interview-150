6f0cdb8ff17f2d0bbe4cb9deaf9fa450b14f8b75
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-02-27 15:57:09 -0800

    Time: 11 ms (72.98%), Space: 17.6 MB (97.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,6 @@
 
+#Time Complexity : O(n), where n is length of s
+#Space Complexity : O(n), where n is length of s
 
 
 class Solution:
@@ -6,4 +8,11 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        return sorted(s) == sorted(t)
+        countS = {}
+        countT = {}
+
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+
+        return countS == countT
