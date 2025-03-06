1a61e9348d5639b45a50de54c6e4e44cc0157e5d
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-03-06 04:46:43 -0800

    Time: 0 ms (100%), Space: 17.7 MB (92.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,17 +4,15 @@ class Solution:
         rows = defaultdict(set)
         boxes = defaultdict(set) 
 
-    
-        for row in range(9):   
+        for row in range(9):
             for col in range(9):
-                if board[row][col] ==".":
+                if board[row][col] == ".":
                     continue
                 if (board[row][col] in rows[row] or
                     board[row][col] in cols[col] or
-                    board[row][col] in boxes[(row//3 , col//3)]):
+                    board[row][col] in boxes[row // 3, col //3]):
                     return False
-                
-                cols[col].add(board[row][col])
                 rows[row].add(board[row][col])
-                boxes[(row//3, col // 3)].add(board[row][col])
+                cols[col].add(board[row][col])
+                boxes[row // 3, col //3].add(board[row][col])
         return True
