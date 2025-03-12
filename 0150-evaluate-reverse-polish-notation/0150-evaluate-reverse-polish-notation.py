d8159cff1f8684baba9641f5588b29dbc2241350
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-03-12 15:38:33 -0700

    Time: 3 ms (64.56%), Space: 19.2 MB (17.45%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,19 +1,30 @@
+#Time Complexity : O(n), where n is length of tokens
+#Space Complexity : O(n), where n is length of tokens.
+#We need to store n elements if aritmetic operations is in the near to last element in the stack
+
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        res = 0
+
         if len(tokens) == 1:
             return int(tokens[0])
 
         operation =  ["+", "-", "*", "/"]
        
         for token in tokens:
-            if token in operation:
-                first = stack.pop()
-                second = stack.pop()
-                res = int(eval(str(second) + token + str(first)))
-                stack.append(res)
+            if token == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif token == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif token == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b)/a))
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
             else:    
                 stack.append(int(token))
-        res = stack.pop()
-        return res 
+      
+        return stack.pop() 
